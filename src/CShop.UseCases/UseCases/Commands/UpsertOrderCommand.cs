4e4869750daa2a55f3a228a7f41f3384b284e67f
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-11 20:08:40 +0700

    update

--- src/CShop.UseCases/UseCases/Commands/UpsertOrderCommand.cs
+++ src/CShop.UseCases/UseCases/Commands/UpsertOrderCommand.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tools.Messaging;
 
 namespace CShop.UseCases.UseCases.Commands;
 public record UpsertOrderCommand(OrderDto Model) : IRequest<OrderDto>
