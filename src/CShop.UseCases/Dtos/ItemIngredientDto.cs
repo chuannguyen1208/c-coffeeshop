8fc1d6c8758abbd7ad8e565cffdec35fd03b4a1c
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-15 14:41:32 +0700

    update

---
+++ src/CShop.UseCases/Dtos/ItemIngredientDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CShop.UseCases.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShop.UseCases.Dtos;
+public class ItemIngredientDto
+{
+    public int Id { get; set; }
+    public int ItemId { get; set; }
+    public int IngredientId { get; set; }
+    public int QuantityRequired { get; set; }
+}
+
+internal class ItemIngrediantProfile : Profile
+{
+    public ItemIngrediantProfile()
+    {
+        CreateMap<ItemIngredient, ItemIngredientDto>().ReverseMap();
+    }
+}
