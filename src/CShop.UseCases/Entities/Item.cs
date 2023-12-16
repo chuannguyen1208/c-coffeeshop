83761d8a0fe6d257f1f9f772815006ab1aff607b
Chuan Nguyen <Chuan.Nguyen@altsourcesoftware.com>
2023-12-16 09:56:50 +0700

    Items img base 64

--- src/CShop.UseCases/Entities/Item.cs
+++ src/CShop.UseCases/Entities/Item.cs
@@ -12,4 +12,5 @@ public class Item
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public string? Img { get; set; }
+    public string? ImgBase64 { get; set; }
 }
